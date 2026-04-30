67bd503d9e0fbfb8777c52b3155a70a6fc112cfd
Madison Held <heldmadison1@gmail.com>
2026-04-30 16:54:10 -0700

    load in required packages and excel data

--- RNA-preservation-analysis.R
+++ RNA-preservation-analysis.R
@@ -1,1 +1,26 @@
 # Insert DV200 Analysis scripts here!
+
+# Install Required Packages
+install.packages(c("readxl", 
+                   "agricolae", 
+                   "car", 
+                   "tidyverse", 
+                   "lme4", 
+                   "AICcmodavg", 
+                   "sjPlot", 
+                   "jtools", 
+                   "betareg"))
+
+# Load Required Packages
+library(readxl)
+library(agricolae)
+library(car)
+library(tidyverse)
+library(lme4)
+library(AICcmodavg)
+library(sjPlot)
+library(jtools)
+library(betareg)
+
+# Load in Sample Data
+All_Samples_FixTests <- read_excel("/Users/heldm/Library/CloudStorage/OneDrive-SharedLibraries-OregonHealth&ScienceUniversity/PC.Conrad Lab - wet lab/Experiment Results/Conrad Bio Archive - CBA/CBA29 - MK27 Processing and NHP Fixative Test 2/All_Samples_FixTests.xlsx")
